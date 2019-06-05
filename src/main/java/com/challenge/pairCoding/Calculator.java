648f51e3217e32b82ebd5404d168bf497f302e6e
Jorge Flores <jorgelizandro55@gmail.com>
2019-06-04 23:15:41 -0600

    Adds Calculator dynamic challenge

---
+++ src/main/java/com/challenge/pairCoding/Calculator.java
@@ -0,0 +1,65 @@
+package com.challenge.pairCoding;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+/**
+ * Given a collection of numbers write a Java program to calculate the sum and product of all even numbers.
+ * How can the program be changed to support a general aggregation of the numbers (like the sum) selected by a general criterion (like the even numbers).
+ */
+
+public final class Calculator {
+    private Calculator() {
+        //NOOP
+    }
+
+    private static <T> T calcEvenNumbersOperation(
+            final Predicate<T> predicate,
+            final BinaryOperator<T> operation,
+            final T... items
+    ) {
+        if (items == null || items.length == 0) {
+            return null;
+        }
+
+        T result = null;
+        for (final T item : items) {
+            if (item != null) {
+                if (predicate.test(item)) {
+                    result = result == null ? item : operation.apply(result, item);
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    public static Integer sumEvenNumbers(final Integer... numbers) {
+        return calcEvenNumbersOperation(
+                i -> (i % 2 == 0),
+                Integer::sum,
+                numbers
+        );
+    }
+
+    public static Integer multiplyEvenNumbers(final Integer... numbers) {
+        return calcEvenNumbersOperation(
+                i -> (i % 2 == 0),
+                (x, y) -> x * y,
+                numbers
+        );
+    }
+
+    public static void main(final String... args) {
+        System.out.println("Sum even numbers: " + sumEvenNumbers(1, 2, 3, 4, 5));
+        System.out.println("Multiply even numbers: " + multiplyEvenNumbers(1, 2, 3, 4, 5));
+
+        //Shorter alternative using streams
+        IntStream.of(1, 2, 3, 4, 5)
+                .filter(x -> x % 2 == 0)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
+    }
+}
